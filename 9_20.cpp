64acdaa300bc9464c0c8e6ff97580e0d903a2e16
Akshay G Bhardwaj <akshaygb@usc.edu>
2016-09-20 18:34:19 -0700

    File name changes and variable name changes.

--- 9_20.cpp
+++ 9_20.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+//#include <cstdio>
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -30,7 +30,7 @@ void a_star(int, int);
 
 int main(int argc, char const *argv[])
 {
-    in = fopen("input03.txt", "r");
+    in = fopen("input16.txt", "r");
     out= fopen("output.txt", "w");
     if(!in || !out)
     {
@@ -50,7 +50,7 @@ int main(int argc, char const *argv[])
     for(int i = 0; i < live_traffic_lines; i++)
     {
         fscanf(in, "%s %s %d", from, to, &time);
-        //printf("%s %s %d \n", from, to, time);
+        printf("%s %s %d \n", from, to, time);
         if(location.count(from) == 0)
         {
             location.insert(make_pair(string(from), location_count));
@@ -100,8 +100,8 @@ int main(int argc, char const *argv[])
 void bfs(int source, int destination)
 {
     cout<<endl<<"BFS : "<<location_inv[source]<<" -> "<<location_inv[destination]<<" ["<<source<<" -> "<<destination<<"]"<<endl;
-    vector <int> tpath;
-    queue <vector <int> > tq;
+    vector <int> path;
+    queue <vector <int> > q;
     int visited[sunday_traffic_lines], path_last_node;
 
     for(int k = 0; k < sunday_traffic_lines; k++)
@@ -109,34 +109,32 @@ void bfs(int source, int destination)
         visited[k] = 0;
     }
     visited[source] = 1;
-    tpath.push_back(source);
-    tq.push(tpath);
-    while(!tq.empty())
+    path.push_back(source);
+    q.push(path);
+    while(!q.empty())
     {
-        tpath = tq.front();
-        tq.pop();
+        path = q.front();
+        q.pop();
 
-        path_last_node = tpath[tpath.size() - 1];
+        path_last_node = path[path.size() - 1];
         if(path_last_node == destination)
         {
-            cout<<"Required path : \n";
-            for (int i = 0; i < tpath.size(); i++)
+            cout<<"The Required path is:: \n";
+            for (int i = 0; i < path.size(); i++)
             {
-                printf("%s %d\n", (char *)location_inv[tpath[i]].c_str(), i);
-                fprintf(out, "%s %d\n", (char *)location_inv[tpath[i]].c_str(), i);
+                printf("%s %d\n", (char *)location_inv[path[i]].c_str(), i);
+                fprintf(out, "%s %d\n", (char *)location_inv[path[i]].c_str(), i);
             }
             return;
         }
 
         for(int i = 0; i < graph[path_last_node].size(); i++)
         {
-            //cout<<">>> "<<path_last_node<<" - "<<graph[path_last_node][i]<<" "<<visited[graph[path_last_node][i]]<<endl;
-            if(!visited[graph[path_last_node][i]] && find(tpath.begin(), tpath.end(), graph[path_last_node][i]) == tpath.end())
+            if(!visited[graph[path_last_node][i]] && find(path.begin(), path.end(), graph[path_last_node][i]) == path.end())
             {
-                //cout<<"visited : "<<graph[path_last_node][i]<<endl;
-                vector <int> new_path(tpath.begin(), tpath.end());
+                vector <int> new_path(path.begin(), path.end());
                 new_path.push_back(graph[path_last_node][i]);
-                tq.push(new_path);
+                q.push(new_path);
                 visited[graph[path_last_node][i]] = 1;
             }
         }
@@ -146,8 +144,8 @@ void bfs(int source, int destination)
 void dfs(int source, int destination)
 {
     cout<<endl<<"DFS : "<<location_inv[source]<<" -> "<<location_inv[destination]<<" ["<<source<<" -> "<<destination<<"]"<<endl;
-    vector <int> tpath;
-    deque <vector <int> > tq;
+    vector <int> path;
+    deque <vector <int> > q;
     int visited[sunday_traffic_lines], path_last_node;
 
     for(int k = 0; k < sunday_traffic_lines; k++)
@@ -155,33 +153,33 @@ void dfs(int source, int destination)
         visited[k] = 0;
     }
     visited[source] = 1;
-    tpath.push_back(source);
-    tq.push_back(tpath);
-    while(!tq.empty())
+    path.push_back(source);
+    q.push_back(path);
+    while(!q.empty())
     {
-        tpath = tq.front();
-        tq.pop_front();
+        path = q.front();
+        q.pop_front();
 
-        path_last_node = tpath[tpath.size() - 1];
+        path_last_node = path[path.size() - 1];
         if(path_last_node == destination)
         {
-            cout<<"Required path : \n";
-            for (int i = 0; i < tpath.size(); i++)
+            cout<<"The Required path is:: \n";
+            for (int i = 0; i < path.size(); i++)
             {
-                printf("%s %d\n", (char *)location_inv[tpath[i]].c_str(), i);
-                fprintf(out, "%s %d\n", (char *)location_inv[tpath[i]].c_str(), i);
+                printf("%s %d\n", (char *)location_inv[path[i]].c_str(), i);
+                fprintf(out, "%s %d\n", (char *)location_inv[path[i]].c_str(), i);
             }
             return;
         }
 
-        deque <vector <int> >::iterator it = tq.begin();
+        deque <vector <int> >::iterator it = q.begin();
         for(int i = 0; i < graph[path_last_node].size(); i++)
         {
-            if(!visited[graph[path_last_node][i]] && find(tpath.begin(), tpath.end(), graph[path_last_node][i]) == tpath.end())
+            if(!visited[graph[path_last_node][i]] && find(path.begin(), path.end(), graph[path_last_node][i]) == path.end())
             {
-                vector <int> new_path(tpath.begin(), tpath.end());
+                vector <int> new_path(path.begin(), path.end());
                 new_path.push_back(graph[path_last_node][i]);
-                it = tq.insert(it, new_path);
+                it = q.insert(it, new_path);
                 it++;
                 visited[graph[path_last_node][i]] = 1;
             }
@@ -192,10 +190,10 @@ void dfs(int source, int destination)
 void ucs(int source, int destination)
 {
     cout<<endl<<"UCS : "<<location_inv[source]<<" -> "<<location_inv[destination]<<" ["<<source<<" -> "<<destination<<"]"<<endl;
-    vector <int> tpath;
+    vector <int> path;
     vector <int> cost;
-    deque <vector <int> > tq;
-    deque <vector <int> > tq1;
+    deque <vector <int> > q;
+    deque <vector <int> > q1;
     int visited[sunday_traffic_lines], path_last_node;
 
     for(int k = 0; k < sunday_traffic_lines; k++)
@@ -203,82 +201,82 @@ void ucs(int source, int destination)
         visited[k] = 0;
     }
     visited[source] = 1;
-    tpath.push_back(source);
+    path.push_back(source);
     cost.push_back(0);
-    tq.push_back(tpath);
-    tq1.push_back(cost);
-    while(!tq.empty())
+    q.push_back(path);
+    q1.push_back(cost);
+    while(!q.empty())
     {
-        tpath = tq.front();
-        tq.pop_front();
-        cost = tq1.front();
-        tq1.pop_front();
+        path = q.front();
+        q.pop_front();
+        cost = q1.front();
+        q1.pop_front();
 
-        path_last_node = tpath[tpath.size() - 1];
+        path_last_node = path[path.size() - 1];
         if(path_last_node == destination)
         {
-            cout<<"Required path : \n";
-            for (int i = 0; i < tpath.size(); i++)
+            cout<<"The Required path is:: \n";
+            for (int i = 0; i < path.size(); i++)
             {
-                printf("%s %d\n", (char *)location_inv[tpath[i]].c_str(), cost[i]);
-                fprintf(out, "%s %d\n", (char *)location_inv[tpath[i]].c_str(), cost[i]);
+                printf("%s %d\n", (char *)location_inv[path[i]].c_str(), cost[i]);
+                fprintf(out, "%s %d\n", (char *)location_inv[path[i]].c_str(), cost[i]);
             }
             return;
         }/*
         else
         {
             cout<<"[ ";
-            for(int i=0;i<tpath.size();++i)
+            for(int i=0;i<path.size();++i)
             {
-                cout<<tpath[i]<<" ";
+                cout<<path[i]<<" ";
             }
             cout<<"]"<<endl;
         }*/
 
         for(int i = 0; i < graph[path_last_node].size(); i++)
         {
-            deque <vector <int> >::iterator it = tq.begin();
-            deque <vector <int> >::iterator it1 = tq1.begin();
-            vector <int> new_path(tpath.begin(), tpath.end());
+            deque <vector <int> >::iterator it = q.begin();
+            deque <vector <int> >::iterator it1 = q1.begin();
+            vector <int> new_path(path.begin(), path.end());
             new_path.push_back(graph[path_last_node][i]);
             vector <int> new_cost(cost.begin(), cost.end());
             int cost_sum = cost[cost.size() - 1] + travel_time[make_pair(path_last_node, graph[path_last_node][i])];
             new_cost.push_back(cost_sum);
             //cout<<">>> "<<path_last_node<<" - "<<graph[path_last_node][i]<<endl;
             //cout<<cost_sum<<endl;
-            if(!visited[graph[path_last_node][i]])// && find(tpath.begin(), tpath.end(), graph[path_last_node][i]) == tpath.end())
+            if(!visited[graph[path_last_node][i]])// && find(path.begin(), path.end(), graph[path_last_node][i]) == path.end())
             {
                 //cout<<"hi "<<cost_sum<<endl;
                 //cout<<"visited : "<<graph[path_last_node][i]<<endl;
                 visited[graph[path_last_node][i]] = 1;
-                if(it1 == tq1.end())
+                if(it1 == q1.end())
                 {
-                    tq.push_back(new_path);
-                    tq1.push_back(new_cost);
+                    q.push_back(new_path);
+                    q1.push_back(new_cost);
                     continue;
                 }
-                while(it1 != tq1.end() && *((*it1).begin()+(*it1).size() - 1) <= cost_sum)
+                while(it1 != q1.end() && *((*it1).begin()+(*it1).size() - 1) <= cost_sum)
                 {
                     it1++;
                     it++;
                 }
-                it = tq.insert(it, new_path);
-                it1 = tq1.insert(it1, new_cost);
+                it = q.insert(it, new_path);
+                it1 = q1.insert(it1, new_cost);
             }
             else
             {
                 //cout<<"hi123 "<<cost_sum<<endl;
-                while(it != tq.end() && graph[path_last_node][i] != *((*it).begin()+(*it).size() - 1))
+                while(it != q.end() && graph[path_last_node][i] != *((*it).begin()+(*it).size() - 1))
                 {
                     it++;
                     it1++;
                 }
-                if(it != tq.end() && cost_sum < *((*it1).begin()+(*it1).size() - 1))
+                if(it != q.end() && cost_sum < *((*it1).begin()+(*it1).size() - 1))
                 {
-                    it = tq.erase(it);
-                    it1 = tq1.erase(it1);
-                    it = tq.insert(it, new_path);
-                    it1 = tq1.insert(it1, new_cost);
+                    it = q.erase(it);
+                    it1 = q1.erase(it1);
+                    it = q.insert(it, new_path);
+                    it1 = q1.insert(it1, new_cost);
                 }
             }
         }
@@ -288,10 +286,10 @@ void ucs(int source, int destination)
 void a_star(int source, int destination)
 {
     cout<<endl<<"A* : "<<location_inv[source]<<" -> "<<location_inv[destination]<<" ["<<source<<" -> "<<destination<<"]"<<endl;
-    deque <vector <int> > tq;
-    deque <vector <int> > tq1;
-    deque <int> tq2;
-    vector <int> tpath;
+    deque <vector <int> > q;
+    deque <vector <int> > q1;
+    deque <int> q2;
+    vector <int> path;
     vector <int> cost;
     int hcost;
     int visited[sunday_traffic_lines], path_last_node;
@@ -301,37 +299,37 @@ void a_star(int source, int destination)
         visited[k] = 0;
     }
     visited[source] = 1;
-    tpath.push_back(source);
+    path.push_back(source);
     cost.push_back(0);
-    tq.push_back(tpath);
-    tq1.push_back(cost);
-    tq2.push_back(sunday_traffic_time[source]);
-    while(!tq.empty())
+    q.push_back(path);
+    q1.push_back(cost);
+    q2.push_back(sunday_traffic_time[source]);
+    while(!q.empty())
     {
-        tpath = tq.front();
-        tq.pop_front();
-        cost = tq1.front();
-        tq1.pop_front();
-        hcost = tq2.front();
-        tq2.pop_front();
+        path = q.front();
+        q.pop_front();
+        cost = q1.front();
+        q1.pop_front();
+        hcost = q2.front();
+        q2.pop_front();
 
-        path_last_node = tpath[tpath.size() - 1];
+        path_last_node = path[path.size() - 1];
         if(path_last_node == destination)
         {
-            cout<<"Required path : \n";
-            for (int i = 0; i < tpath.size(); i++)
+            cout<<"The Required path is:: \n";
+            for (int i = 0; i < path.size(); i++)
             {
-                printf("%s %d\n", (char *)location_inv[tpath[i]].c_str(), cost[i]);
-                fprintf(out, "%s %d\n", (char *)location_inv[tpath[i]].c_str(), cost[i]);
+                printf("%s %d\n", (char *)location_inv[path[i]].c_str(), cost[i]);
+                fprintf(out, "%s %d\n", (char *)location_inv[path[i]].c_str(), cost[i]);
             }
             return;
         }/*
         else
         {
             cout<<"[ ";
-            for(int i=0;i<tpath.size();++i)
+            for(int i=0;i<path.size();++i)
             {
-                cout<<tpath[i]<<" ";
+                cout<<path[i]<<" ";
             }
             //cout<<"]"<<endl;
             cout<<"] "<<cost[cost.size() - 1]<<" "<<hcost<<endl;
@@ -339,10 +337,10 @@ void a_star(int source, int destination)
 
         for(int i = 0; i < graph[path_last_node].size(); i++)
         {
-            deque <vector <int> >::iterator it = tq.begin();
-            deque <vector <int> >::iterator it1 = tq1.begin();
-            deque <int>::iterator it2 = tq2.begin();
-            vector <int> new_path(tpath.begin(), tpath.end());
+            deque <vector <int> >::iterator it = q.begin();
+            deque <vector <int> >::iterator it1 = q1.begin();
+            deque <int>::iterator it2 = q2.begin();
+            vector <int> new_path(path.begin(), path.end());
             new_path.push_back(graph[path_last_node][i]);
             vector <int> new_cost(cost.begin(), cost.end());
             int cost_sum = cost[cost.size() - 1] + travel_time[make_pair(path_last_node, graph[path_last_node][i])];
@@ -350,45 +348,45 @@ void a_star(int source, int destination)
             int new_hcost = cost_sum + sunday_traffic_time[graph[path_last_node][i]];
             //cout<<">>> "<<path_last_node<<" - "<<graph[path_last_node][i]<<endl;
             //cout<<cost_sum<<" "<<new_hcost<<endl;
-            if(!visited[graph[path_last_node][i]])// && find(tpath.begin(), tpath.end(), graph[path_last_node][i]) == tpath.end())
+            if(!visited[graph[path_last_node][i]])// && find(path.begin(), path.end(), graph[path_last_node][i]) == path.end())
             {
                 //cout<<"hi "<<cost_sum<<endl;
                 //cout<<"visited : "<<graph[path_last_node][i]<<endl;
                 visited[graph[path_last_node][i]] = 1;
-                if(it2 == tq2.end())
+                if(it2 == q2.end())
                 {
-                    tq.push_back(new_path);
-                    tq1.push_back(new_cost);
-                    tq2.push_back(new_hcost);
+                    q.push_back(new_path);
+                    q1.push_back(new_cost);
+                    q2.push_back(new_hcost);
                     continue;
                 }
-                while(it2 != tq2.end() && (*it2) <= new_hcost)
+                while(it2 != q2.end() && (*it2) <= new_hcost)
                 {
                     it++;
                     it1++;
                     it2++;
                 }
-                it = tq.insert(it, new_path);
-                it1 = tq1.insert(it1, new_cost);
-                it2 = tq2.insert(it2, new_hcost);
+                it = q.insert(it, new_path);
+                it1 = q1.insert(it1, new_cost);
+                it2 = q2.insert(it2, new_hcost);
             }
             else
             {
                 //cout<<"hi123 "<<cost_sum<<endl;
-                while(it != tq.end() && graph[path_last_node][i] != *((*it).begin()+(*it).size() - 1))
+                while(it != q.end() && graph[path_last_node][i] != *((*it).begin()+(*it).size() - 1))
                 {
                     it++;
                     it1++;
                     it2++;
                 }
-                if(it != tq.end() && cost_sum < *((*it1).begin()+(*it1).size() - 1))
+                if(it != q.end() && cost_sum < *((*it1).begin()+(*it1).size() - 1))
                 {
-                    it = tq.erase(it);
-                    it1 = tq1.erase(it1);
-                    it2 = tq2.erase(it2);
-                    it = tq.insert(it, new_path);
-                    it1 = tq1.insert(it1, new_cost);
-                    it2 = tq2.insert(it2, new_hcost);
+                    it = q.erase(it);
+                    it1 = q1.erase(it1);
+                    it2 = q2.erase(it2);
+                    it = q.insert(it, new_path);
+                    it1 = q1.insert(it1, new_cost);
+                    it2 = q2.insert(it2, new_hcost);
                 }
             }
         }
